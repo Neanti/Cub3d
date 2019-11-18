e10f01a2b3ef8951fe75fc9275f0b61155f55fa4
Neanti <audric00@live.fr>
2019-11-18 18:08:25 +0100

    setup

--- main.c
+++ main.c
@@ -2,9 +2,38 @@
 #include <stdio.h>
 #include <string.h>
 #include <mlx.h>
+#include "minilibx/mlx.h"
+#include <sys/types.h>
+#include <math.h>
+
 
 #define RED get_color(255,0,0)
 #define ORANGE get_color(255,128,0)
+
+size_t strlcpy(char *dst, const char *src, size_t dsize)
+{
+    const char *osrc = src;
+    size_t nleft = dsize;
+
+    /* Copy as many bytes as will fit. */
+    if (nleft != 0) {
+        while (--nleft != 0) {
+            if ((*dst++ = *src++) == '\0')
+                break;
+        }
+    }
+
+    /* Not enough room in dst, add NUL and traverse rest of src. */
+    if (nleft == 0) {
+        if (dsize != 0)
+            *dst = '\0';		/* NUL-terminate dst */
+        while (*src++)
+            ;
+    }
+
+    return(src - osrc - 1);	/* count does not include NUL */
+}
+
 typedef struct    data_s
 {
     void          *mlx_ptr;
@@ -43,6 +72,8 @@ int get_color(int r, int g, int b)
 int main(void)
 {
     data_t        data;
+    int x = 1;
+    int y = 1;
 
     if ((data.mlx_ptr = mlx_init()) == NULL)
         return (EXIT_FAILURE);
@@ -52,6 +83,8 @@ int main(void)
     put_square(data.mlx_ptr, data.mlx_win, 8,8,50,50, get_color(255,255,255));
     put_square(data.mlx_ptr, data.mlx_win, 300,300,350,350, RED);
     mlx_string_put(data.mlx_ptr, data.mlx_win, 60, 60, RED, "yo les bitches\n");
+    void *img = mlx_xpm_file_to_image(data.mlx_ptr, "test.xpm", &x, &y);
+    mlx_put_image_to_window(data.mlx_ptr, data.mlx_win, img, 100, 100);
     mlx_loop(data.mlx_ptr);
     return (EXIT_SUCCESS);
 }
