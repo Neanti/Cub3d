0df84c62c97d626793b000457d084ef5305c1476
Neanti <audric00@live.fr>
2019-11-22 14:05:12 +0100

    home

--- main.c
+++ main.c
@@ -40,23 +40,16 @@ typedef struct    data_s
     void          *mlx_win;
 }                 data_t;
 
-int put_square(void *mlx_ptr, void *win_ptr, int x1, int y1, int x2, int y2, int color)
-{
-    int i = x1;
-    int j = y1;
+#define mapWidth 4
+#define mapHeight 4
 
-    while(i < x2)
-    {
-        while(j < y2)
+int worldMap[mapWidth][mapHeight]=
         {
-            mlx_pixel_put(mlx_ptr, win_ptr, i, j, color);
-            j++;
-        }
-        j = x1;
-        i++;
-    }
-    return (0);
-}
+                {1,1,1,1},
+                {1,0,0,1},
+                {1,0,2,1},
+                {1,1,1,1}
+        };
 
 int get_color(int r, int g, int b)
 {
@@ -69,22 +62,112 @@ int get_color(int r, int g, int b)
     return (o);
 }
 
+typedef struct s_point
+{
+    int x;
+    int y;
+}               t_point;
+
+typedef struct s_player
+{
+    int Px;
+    int Py;
+    int size;
+    int FOV;
+    double ori;
+}               t_player;
+
+t_player prepare_player(void)
+{
+    t_player p;
+    p.ori = 0;
+    p.FOV = 60;
+    p.size = 32;
+    p.Px = 0;
+    p.Py = 0;
+    return (p);
+}
+
+typedef struct s_plan
+{
+    int x;
+    int y;
+    double ang;
+    double dist;
+    double act;
+}               t_plan;
+
+t_plan prepare_plan(void)
+{
+    t_plan p;
+    p.x = 320;
+    p.y = 200;
+    p.ang = 0;
+    p.dist = 0;
+    p.act = -30;
+    return (p);
+}
+
+void print_point(t_point p)
+{
+    printf("---tpoint---\n x=%i et y=%i\n---fin tpoint---\n");
+}
+
+void print_plan(t_plan p)
+{
+    printf("---tplan---\nx=%i et y=%i et angle=%f et dist=%f\n---fin tplan---\n", p.x, p.y, p.ang, p.dist);
+}
+
+double degree2radian(double d)
+{
+    double r;
+    r = d * (M_PI / 180);
+    return (r);
+}
+
+void print_player(t_player p)
+{
+    printf("---tplayer---\nPx=%i et Py=%i et size=%i et FOV=%i et ori=%f\n---fin tplayer---\n", p.Px, p.Py, p.size, p.FOV, p.ori);
+}
+
+void get_plan_info(t_player player,t_plan *tplan)
+{
+    double ang;
+    double x;
+
+    ang = player.FOV / 2;
+    x = (double)tplan->x / (double)2;
+    printf("%f / %f=tan(%f) == %f\n", x, tan(degree2radian(ang)), ang, x / (tan(degree2radian(ang))));
+    tplan->dist = x / (tan(degree2radian(ang)));
+    tplan->ang = (double)player.FOV / (double)tplan->x;
+}
+
+t_point calc_dH(t_player player, t_plan plan)
+{
+    t_point p;
+    int comp;
+
+    if (plan.act < 0)
+
+}
+
 int main(void)
 {
     data_t        data;
     int x = 1;
     int y = 1;
-
     if ((data.mlx_ptr = mlx_init()) == NULL)
         return (EXIT_FAILURE);
     if ((data.mlx_win = mlx_new_window(data.mlx_ptr, 640, 480, "Hello world")) == NULL)
         return (EXIT_FAILURE);
-    int a = 555555555;
-    put_square(data.mlx_ptr, data.mlx_win, 8,8,50,50, get_color(255,255,255));
-    put_square(data.mlx_ptr, data.mlx_win, 300,300,350,350, RED);
-    mlx_string_put(data.mlx_ptr, data.mlx_win, 60, 60, RED, "yo les bitches\n");
-    void *img = mlx_xpm_file_to_image(data.mlx_ptr, "test.xpm", &x, &y);
-    mlx_put_image_to_window(data.mlx_ptr, data.mlx_win, img, 100, 100);
+    t_plan plan = prepare_plan();
+    t_player player = prepare_player();
+    print_plan(plan);
+    print_player(player);
+    get_plan_info(player, &plan);
+    print_plan(plan);
+    t_point pointH = calc_dH(player);
+    print_point(pointH);
     mlx_loop(data.mlx_ptr);
     return (EXIT_SUCCESS);
 }
