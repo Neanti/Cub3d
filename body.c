c138d15920ea5504ac570b4a6070e84ec2131570
Audric GAY <augay@e2r13p7.42.fr>
2020-01-16 17:53:49 +0100

    suitee

--- body.c
+++ body.c
@@ -33,8 +33,10 @@ double	find_wallx(int side, t_wrap *wrap, double wall_dist, int x)
 	double raydir_x;
 	double raydir_y;
 
-	raydir_x = wrap->game->dirX + wrap->game->planeX * (2 * x / (double)wrap->cub->rw - 1);
-	raydir_y = wrap->game->dirY + wrap->game->planeY * (2 * x / (double)wrap->cub->rw - 1);
+	raydir_x = wrap->game->dirX + wrap->game->planeX * (2 *
+	x / (double)wrap->cub->rw - 1);
+	raydir_y = wrap->game->dirY + wrap->game->planeY * (2 *
+	x / (double)wrap->cub->rw - 1);
 	if (side == 0)
 		wallx = wrap->game->posY + wall_dist * raydir_y;
 	else
@@ -43,26 +45,30 @@ double	find_wallx(int side, t_wrap *wrap, double wall_dist, int x)
 	return (wallx);
 }
 
-double	*deal_wall_one(t_wrap *wrap, double wdist, int side, int x, int hit, double zbuffer[wrap->cub->rw])
+double	*deal_wall_one(t_wrap *wrap, double zbuffer[wrap->cub->rw], int t[7], double d[7])
 {
-	int l_height;
-	double wallx;
-	double dir_x;
-	double dir_y;
-	int tex_x;
+	int		l_height;
+	double	wallx;
+	double	dir_x;
+	double	dir_y;
+	int		tex_x;
 
-	l_height = (int)(wrap->cub->rh / wdist);
-	dir_x = wrap->game->dirX + wrap->game->planeX * (2 * x / (double)wrap->cub->rw - 1);
-	dir_y = wrap->game->dirY + wrap->game->planeY * (2 * x / (double)wrap->cub->rw - 1);
-	wallx = find_wallx(side, wrap, wdist, x);
+	l_height = (int)(wrap->cub->rh / d[6]);
+	dir_x = wrap->game->dirX + wrap->game->planeX *
+	(2 * t[6] / (double)wrap->cub->rw - 1);
+	dir_y = wrap->game->dirY + wrap->game->planeY *
+	(2 * t[6] / (double)wrap->cub->rw - 1);
+	wallx = find_wallx(t[5], wrap, d[6], t[6]);
 	tex_x = (int)(wallx * (double)wrap->img[4].nbc);
-	if (side == 0 && dir_x > 0)
+	if (t[5] == 0 && dir_x > 0)
 		tex_x = wrap->img[0].nbc - tex_x - 1;
-	if (side == 1 && dir_y < 0)
+	if (t[5] == 1 && dir_y < 0)
 		tex_x = wrap->img[0].nbc - tex_x - 1;
-	if (hit == 1)
-		ft_put_line(ft_pack(x, -l_height / 2 + wrap->cub->rh / 2, l_height / 2 + wrap->cub->rh / 2, wrap->cub->rh), nbt(side, dir_x, dir_y), wallx, wrap);
-	zbuffer[x] = wdist;
+	if (t[5] == 1)
+		ft_put_line(ft_pack(t[6], -l_height / 2 + wrap->cub->rh / 2, l_height / 2
+		+ wrap->cub->rh / 2, wrap->cub->rh),
+		nbt(t[5], dir_x, dir_y), wallx, wrap); // CHECK X
+	zbuffer[t[6]] = d[6];
 	return (zbuffer);
 }
 
@@ -78,102 +84,129 @@ double	calc_wall(int map, double pos, int step, double raydir)
 	return ((map - pos + (1 - step) / 2) / raydir);
 }
 
+void	prep_no(t_wrap *wrap, int t[6])
+{
+	t[0] = (int)wrap->game->posX;
+	t[1] = (int)wrap->game->posY;
+	t[4] = 0;
+}
+
+void	prep_nod(t_wrap *wrap, double d[7], int x, int t[7])
+{
+	d[0] = wrap->game->dirX + wrap->game->planeX * (2 *
+	x / (double)wrap->cub->rw - 1);
+	d[1] = wrap->game->dirY + wrap->game->planeY * (2 *
+	x / (double)wrap->cub->rw - 1);
+	d[4] = fabs(1 / d[0]);
+	d[5] = fabs(1 / d[1]);
+	if (d[0] < 0 && (t[2] = -1))
+		d[2] = (wrap->game->posX - t[0]) * d[4];
+	else if ((t[2] = 1))
+		d[2] = (t[0] + 1.0 - wrap->game->posX) * d[4];
+	if (d[1] < 0)
+	{
+		t[3] = -1;
+		d[3] = (wrap->game->posY - t[1]) * d[5];
+	}
+	else if ((t[3] = 1))
+		d[3] = (t[1] + 1.0 - wrap->game->posY) * d[5];
+	t[6] = x;
+}
+
 double	*deal_wall(t_wrap *wrap, int x, double zbuffer[wrap->cub->rw])
 {
-	double raydir_x;
-	double raydir_y;
-	int map_x;
-	int map_y;
-	double sdist_x;
-	double sdist_y;
-	double ddist_x;
-	double ddist_y;
-	double wall_dist;
-	int step_x;
-	int step_y;
-	int hit;
-	int side;
+	int		t[7];
+	double	d[7];
 
-	raydir_x = wrap->game->dirX + wrap->game->planeX * (2 * x / (double)wrap->cub->rw - 1);
-	raydir_y = wrap->game->dirY + wrap->game->planeY * (2 * x / (double)wrap->cub->rw - 1);
-	map_x = (int)wrap->game->posX;
-	map_y = (int)wrap->game->posY;
-	ddist_x = fabs(1 / raydir_x);
-	ddist_y = fabs(1 / raydir_y);
-	hit = 0;
-	if (raydir_x < 0 && (step_x = -1))
-		sdist_x = (wrap->game->posX - map_x) * ddist_x;
-	else if ((step_x = 1))
-		sdist_x = (map_x + 1.0 - wrap->game->posX) * ddist_x;
-	if (raydir_y < 0)
+	prep_no(wrap, t);
+	prep_nod(wrap, d, x, t);
+	while (t[4] == 0)
 	{
-		step_y = -1;
-		sdist_y = (wrap->game->posY - map_y) * ddist_y;
-	}
-	else if ((step_y = 1))
-		sdist_y = (map_y + 1.0 - wrap->game->posY) * ddist_y;
-	while (hit == 0)
-	{
-		if (sdist_x < sdist_y && !(side = 0))
-			deal_side(&sdist_x, ddist_x, &map_x, step_x);
+		if (d[2] < d[3] && !(t[5] = 0))
+			deal_side(&d[2], d[4], &t[0], t[2]);
 		else
-			side = deal_side(&sdist_y, ddist_y, &map_y, step_y);
-		hit = (wrap->cub->map[map_x][map_y] - 48 == 1) ? 1 : hit;
-		wall_dist = (side == 0) ? calc_wall(map_x, wrap->game->posX, step_x, raydir_x) : calc_wall(map_y, wrap->game->posY, step_y, raydir_y);
-		deal_wall_one(wrap, wall_dist, side, x, hit, zbuffer);
+			t[5] = deal_side(&d[3], d[5], &t[1], t[3]);
+		t[4] = (wrap->cub->map[t[0]][t[1]] - 48 == 1) ? 1 : t[4];
+		d[6] = (t[5] == 0) ? calc_wall(t[0], wrap->game->posX, t[2], d[0]) :
+		calc_wall(t[1], wrap->game->posY, t[3], d[1]);
+		deal_wall_one(wrap, zbuffer, t, d);
 	}
 	return (zbuffer);
 }
 
+void	prep_spd(t_wrap *wrap, double d[2], int i, int t[14])
+{
+	double d0;
+	double d1;
+	double d2;
+
+	d0 = wrap->cub->sx[i] + 0.5 - wrap->game->posX;
+	d1 = wrap->cub->sy[i] + 0.5 - wrap->game->posY;
+	d2 = 1.0 / (wrap->game->planeX * wrap->game->dirY - wrap->game->dirX
+	* wrap->game->planeY);
+	d[0] = d2 * (wrap->game->dirY * d0 - wrap->game->dirX * d1);
+	d[1] = d2 * (-1 * wrap->game->planeY * d0 + wrap->game->planeX * d1);
+	//prep_spi(wrap, t, d);
+}
+
+void	prep_spi(t_wrap *wrap, int t[14], double d[2])
+{
+	t[0] = (int)((wrap->cub->rw / 2) * (1 + d[0] / d[1]));
+	t[1] = (int)(0.0 / d[1]);
+	t[2] = abs((int)(wrap->cub->rh / d[1]));
+	t[3] = -1 * t[2] / 2 + wrap->cub->rh / 2 + t[1];
+	if (t[3] < 0)
+		t[3] = 0;
+	t[4] = t[2] / 2 + wrap->cub->rh / 2 + t[1];
+	if (t[4] >= wrap->cub->rh)
+		t[4] = wrap->cub->rh;
+	t[5] = abs((int)(wrap->cub->rh / d[1]));
+	t[6] = -1 * t[5] / 2 + t[0];
+	if (t[6] < 0)
+		t[6] = 0;
+	t[7] = t[5] / 2 + t[0];
+	if (t[7] >= wrap->cub->rw)
+		t[7] = wrap->cub->rw - 1;
+	t[8] = t[6];
+}
+
 void	deal_sprite(t_wrap *wrap, int i, double *zbuffer)
 {
-	double sprite_x = wrap->cub->sx[i] + 0.5 - wrap->game->posX;
-	double sprite_y = wrap->cub->sy[i] + 0.5 - wrap->game->posY;
-	double i_det = 1.0 / (wrap->game->planeX * wrap->game->dirY - wrap->game->dirX * wrap->game->planeY);
-	double transform_x = i_det * (wrap->game->dirY * sprite_x - wrap->game->dirX * sprite_y);
-	double transform_y = i_det * (-1 * wrap->game->planeY * sprite_x + wrap->game->planeX * sprite_y);
-	int sp_sc_x = (int)((wrap->cub->rw / 2) * (1 + transform_x / transform_y));
-	int v_move_sc = (int)(0.0 / transform_y);
-	int spr_height = abs((int)(wrap->cub->rh / transform_y));
-	int d_start_y = -1 * spr_height / 2 + wrap->cub->rh / 2 + v_move_sc;
-	if (d_start_y < 0)
-		d_start_y = 0;
-	int d_end_y = spr_height / 2 + wrap->cub->rh / 2 + v_move_sc;
-	if (d_end_y >= wrap->cub->rh)
-		d_end_y = wrap->cub->rh;
-	int spr_width = abs((int)(wrap->cub->rh / transform_y));
-	int d_start_x = -1 * spr_width / 2 + sp_sc_x;
-	if (d_start_x < 0)
-		d_start_x = 0;
-	int d_end_x = spr_width / 2 + sp_sc_x;
-	if (d_end_x >= wrap->cub->rw)
-		d_end_x = wrap->cub->rw - 1;
-	for (int stripe = d_start_x; stripe < d_end_x + 1; stripe++)
+	double	d[2];
+	int		t[14];
+
+	prep_spd(wrap, d, i, t);
+	prep_spi(wrap, t,d);
+	while (t[8] < t[7] + 1)
 	{
-		int tex_x = (int)(256 * (stripe - (-spr_width / 2 + sp_sc_x)) * (wrap->img[4].nbc) / spr_width) / 256;
-		if (transform_y > 0 && stripe >= 0 && stripe < wrap->cub->rw && transform_y < zbuffer[stripe])
+		t[9] = (int)(256 * (t[8] - (-t[5] / 2 + t[0])) *
+		(wrap->img[4].nbc) / t[5]) / 256;
+		if (d[1] > 0 && t[8] >= 0 && t[8] < wrap->cub->rw &&
+		d[1] < zbuffer[t[8]])
 		{
-			int y = d_start_y;
-			while (y < d_end_y)
+			t[12] = t[3];
+			while (t[12] < t[4])
 			{
-				int d = (y - v_move_sc) * 256 - wrap->cub->rh * 128 + spr_height * 128;
-				int tex_y = ((d * (wrap->img[4].nbl - 1)) / spr_height) / 256;
-				int color = pick_color(wrap->img[4], tex_y + 1, tex_x + 1);
-				if (color >= 0)
-					mlx_pixel_put(wrap->data->mlx_ptr, wrap->data->mlx_win, stripe, y, color);
-				y++;
+				t[11] = (t[12] - t[1]) * 256 - wrap->cub->rh * 128 + t[2] * 128;
+				t[10] = ((t[11] * (wrap->img[4].nbl - 1)) / t[2]) / 256;
+				t[13] = pick_color(wrap->img[4], t[10] + 1, t[9] + 1);
+				if (t[13] >= 0)
+					mlx_pixel_put(wrap->data->mlx_ptr, wrap->data->mlx_win,
+					t[8], t[12], t[13]);
+				t[12]++;
 			}
 		}
+		t[8]++;
 	}
 }
 
 void	fct_test(t_wrap *wrap)
 {
-	int w;
-	double pos_x;
-	double pos_y;
-	double zbuffer[wrap->cub->rw];
-	int x;
+	int		w;
+	double	pos_x;
+	double	pos_y;
+	double	zbuffer[wrap->cub->rw];
+	int		x;
 
 	w = wrap->cub->rw;
 	pos_x = wrap->game->posX;
